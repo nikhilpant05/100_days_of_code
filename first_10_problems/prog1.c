d18f9a664a6df09febe28aa419314f4bb85fd604
nikhilpant05 <niksuniverse05@gmail.com>
2025-08-23 15:37:57 +0530

    change

--- first_10_problems/prog1.c
+++ first_10_problems/prog1.c
@@ -1,5 +1,4 @@
 // 1. Write a program to input two numbers and display their sum.
-
 #include<stdio.h>
 
 int main(){
--- first_10_problems/prog3.c
+++ first_10_problems/prog3.c
@@ -1,6 +1,16 @@
 // 3. Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
-
 #include<stdio.h>
-int main(){
 
+int main(){
+    int l=10;
+    int b=20;
+    int perimeter,area;
+
+    perimeter=2*(l+b);
+    printf("\nThe perimeter of the rectangle is : %d",perimeter);
+
+    area=l*b;
+    printf("\nThe area of the rectangle is : %d",area);
+
+    return 0;
 }
