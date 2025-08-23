0e3df4f60aeb2bf1a4e00254b3339e37a0c6637d
nikhilpant05 <niksuniverse05@gmail.com>
2025-08-23 15:13:51 +0530

    changed

---
+++ first_10_problems/prog2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+    int a,b,sum,difference,product,quotient;
+    printf("Enter two numbers : ");
+    scanf("%d%d",&a,&b);
+    
+    sum=a+b;
+    printf("The sum of the numbers is : %d",sum);
+
+    difference=a-b;
+    printf("\nThe difference of the numbers is : %d",difference);
+
+    product=a*b;
+    printf("\nThe product of the numbers is : %d",product);
+
+    quotient=a/b;
+    printf("\nThe quotient of the numbers is : %d",quotient);
+
+    return 0;
+    
+}
