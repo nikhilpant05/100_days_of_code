f6f9a98faea342a87084e51f00dc2447c2bfbb52
nikhilpant05 <niksuniverse05@gmail.com>
2025-09-04 23:01:09 +0530

    done

--- 11-20 problems - Copy/20_prog.c
+++ 11-20 problems - Copy/20_prog.c
@@ -4,31 +4,36 @@
 
 int main() {
     int day;
-    printf("Enter a number (1 to 7): ");
+
+    printf("Enter a number (1-7): ");
     scanf("%d", &day);
-    if (day == 1) {
-        printf("Monday\n");
+    
+    switch(day) {
+        case 1:
+            printf("Sunday\n");
+            break;
+        case 2:
+            printf("Monday\n");
+            break;
+        case 3:
+            printf("Tuesday\n");
+            break;
+        case 4:
+            printf("Wednesday\n");
+            break;
+        case 5:
+            printf("Thursday\n");
+            break;
+        case 6:
+            printf("Friday\n");
+            break;
+        case 7:
+            printf("Saturday\n");
+            break;
+        default:
+            printf("Invalid input! Please enter a number between 1 and 7.\n");
+            break;
     }
-    else if (day == 2) {
-        printf("Tuesday\n");
-    }
-    else if (day == 3) {
-        printf("Wednesday\n");
-    }
-    else if (day == 4) {
-        printf("Thursday\n");
-    }
-    else if (day == 5) {
-        printf("Friday\n");
-    }
-    else if (day == 6) {
-        printf("Saturday\n");
-    }
-    else if (day == 7) {
-        printf("Sunday\n");
-    }
-    else {
-        printf("Invalid input! Please enter a number between 1 and 7.\n");
-    }
+
     return 0;
 }
