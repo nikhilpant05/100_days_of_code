eac4fa3ef92cae0573525950795fb881af1848f1
nikhilpant05 <niksuniverse05@gmail.com>
2025-10-20 20:42:07 +0530

    done

--- 11-20 problems/19_prog.c
+++ 11-20 problems/19_prog.c
@@ -8,21 +8,14 @@ int main() {
     printf("Enter the three sides of the triangle: ");
     scanf("%d %d %d", &a, &b, &c);
 
-    if (a + b > c && a + c > b && b + c > a) {
-
-        if (a == b && b == c) {
-            printf("Equilateral Triangle\n");
-        }
-        else if (a == b || b == c || a == c) {
-            printf("Isosceles Triangle\n");
-        }
-        else {
-            printf("Scalene Triangle\n");
-        }
-    }
-    else {
-        printf("Not a valid triangle\n");
-    }
-
+    if (a == b && b == c) 
+    printf("Equilateral Triangle\n");
+    
+    else if (a == b || b == c || a == c) 
+    printf("Isosceles Triangle\n");
+    
+    else 
+    printf("Scalene Triangle\n");
+        
     return 0;
 }
